39d2d1e46cf39851dcda016216db66a16a38136e
iraklikeche <irakli.kechee@gmail.com>
2024-03-18 18:12:32 +0400

    refactor:remove about page

--- src/router/index.js
+++ src/router/index.js
@@ -10,15 +10,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/:catchAll(.*)', // This will match all paths that aren't matched by above routes
+      path: '/:catchAll(.*)',
       name: 'notfound',
       component: () => import('../views/errors/notFound404.vue')
     }
