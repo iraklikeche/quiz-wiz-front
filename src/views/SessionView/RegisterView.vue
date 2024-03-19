c4a8dfd7654ee83be8d31885539dc1578d03ceac
iraklikeche <irakli.kechee@gmail.com>
2024-03-19 23:19:53 +0400

    refactor:extract inputs in separate component& install axios

--- src/views/SessionView/RegisterView.vue
+++ src/views/SessionView/RegisterView.vue
@@ -5,55 +5,50 @@
       :buttonName="'Log in'"
       class="block sm:hidden text-center sm:text-left mb-12 sm:mb-0"
     />
-    <form class="flex flex-col gap-5 max-w-[26rem]">
-      <!-- If it's okay I will leave it so, till I get familiar with vee-validate and later I will extract input and label into separate components to make it more clean and DRY.  -->
-      <div class="flex flex-col">
-        <label class="text-custom-gray text-sm mb-1">Username</label>
-        <input
-          type="text"
-          placeholder="Your username"
-          class="border border-border-gray py-4 px-4 rounded-xl focus:border-4 outline-none"
-        />
-      </div>
-      <div class="flex flex-col">
-        <label class="text-custom-gray text-sm mb-1">Email</label>
-        <input
-          type="email"
-          placeholder="example@gmail.com"
-          class="border border-border-gray py-4 px-4 rounded-xl focus:border-4 outline-none"
-        />
-      </div>
-      <div class="flex flex-col relative">
-        <label class="text-custom-gray text-sm mb-1">Password</label>
-        <input
-          :type="isPasswordVisible ? 'text' : 'password'"
-          placeholder="Must be 3 characters"
-          class="border border-border-gray py-4 px-4 rounded-xl focus:border-4 outline-none"
-        />
-        <ShowPassword
-          :customClass="'absolute top-[55%] right-[4%] cursor-pointer'"
-          @toggle="togglePassword"
-        />
-      </div>
-      <div class="flex flex-col relative">
-        <label class="text-custom-gray text-sm mb-1">Confirm Password</label>
-        <input
-          :type="isConfirmPasswordVisible ? 'text' : 'password'"
-          placeholder="Must be 3 characters"
-          class="border border-border-gray py-4 px-4 rounded-xl focus:border-4 outline-none"
-        />
-        <ShowPassword
-          :customClass="'absolute top-[55%] right-[4%] cursor-pointer'"
-          @toggle="toggleConfirmPassword"
-        />
-      </div>
+    <Form @submit="onSubmit" class="flex flex-col gap-5 max-w-[26rem]" v-slot="{ values }">
+      <CustomInput
+        label="Username"
+        name="username"
+        placeholder="Your username"
+        rules="required|min:3"
+        type="text"
+      />
 
-      <div class="flex items-center gap-4 mt-2 pl-1">
-        <input type="radio" class="scale-150" />
+      <CustomInput
+        label="Email"
+        name="email"
+        placeholder="example@gmail.com"
+        rules="required|email"
+        type="email"
+      />
+
+      <CustomInput
+        type="password"
+        label="Password"
+        name="password"
+        placeholder="Must be 3 characters"
+        rules="required|min:3"
+        isPasswordField
+      />
+
+      <CustomInput
+        type="password"
+        label="Confirm Password"
+        name="confirmation"
+        placeholder="Must be 3 characters"
+        rules="confirmed:@password"
+        isPasswordField
+      />
+
+      <div class="flex items-center gap-4 pl-1">
+        <Field name="terms" type="radio" value="true" class="scale-150" rules="required" />
         <label class="text-[#344054] text-sm">I accept the terms and privacy policy</label>
       </div>
+      <ErrorMessage name="terms" />
+      <pre>{{ values }}</pre>
+
       <button class="bg-black text-white py-4 rounded-xl mt-6 font-semibold">Sign Up</button>
-    </form>
+    </Form>
 
     <AccountLinks
       :question="'Already have account? '"
@@ -70,6 +65,14 @@ import Logo from '@/components/icons/Logo.vue'
 import GoBack from '@/components/icons/GoBack.vue'
 import SessionLayout from '@/components/SessionLayout.vue'
 import AccountLinks from '@/components/AccountLinks.vue'
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import { defineRule } from 'vee-validate'
+import * as AllRules from '@vee-validate/rules'
+import CustomInput from '@/components/form/CustomInput.vue'
+
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule])
+})
 
 export default {
   components: {
@@ -77,7 +80,11 @@ export default {
     Logo,
     GoBack,
     SessionLayout,
-    AccountLinks
+    AccountLinks,
+    Form,
+    Field,
+    ErrorMessage,
+    CustomInput
   },
 
   data() {
@@ -91,16 +98,6 @@ export default {
     onSubmit(values) {
       console.log(JSON.stringify(values, null, 2))
     },
-    validateEmail(value) {
-      if (!value) {
-        return 'This field is required'
-      }
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-      if (!regex.test(value)) {
-        return 'This field must be a valid email'
-      }
-      return true
-    },
     togglePassword() {
       this.isPasswordVisible = !this.isPasswordVisible
     },
@@ -109,6 +106,9 @@ export default {
     },
     goBack() {
       this.$router.go(-1)
+    },
+    emitToggle() {
+      this.$emit('toggle')
     }
   }
 }
