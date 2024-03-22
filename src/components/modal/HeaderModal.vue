5d19b0968cb73515953fa3ece3004d9cbe8b953d
iraklikeche <irakli.kechee@gmail.com>
2024-03-22 23:13:21 +0400

    remove unnecessary imports

--- src/components/modal/HeaderModal.vue
+++ src/components/modal/HeaderModal.vue
@@ -18,15 +18,6 @@
       Sign Up
     </button>
 
-    <!-- <TheModal
-      :name="'slide-up'"
-      :show="modals.registration"
-      @update:show="toggleModal('registration', $event)"
-      :modalContentClasses="'bg-white w-full rounded-t-lg p-4 transition-transform'"
-      class="mt-20"
-    >
-      <RegisterView />
-    </TheModal> -->
 
     <button
       @click="toggleModal('login', true)"
@@ -35,39 +26,18 @@
       Log in
     </button>
 
-    <!-- <TheModal
-      :name="'slide-up'"
-      :show="modals.login"
-      @update:show="toggleModal('login', $event)"
-      :modalContentClasses="'bg-white w-full rounded-t-lg p-4 transition-transform'"
-      class="mt-20"
-    >
-      <LoginView />
-    </TheModal> -->
-  </div>
 </template>
 
 <script>
 import Logo from '@/components/icons/Logo.vue'
 import CloseModalBtn from '@/components/icons/CloseModalBtn.vue'
-import RegisterView from '@/views/SessionView/RegisterView.vue'
-import LoginView from '@/views/SessionView/LoginView.vue'
-import TheModal from '@/components/modal/TheModal.vue'
 
 export default {
   components: {
-    TheModal,
-    RegisterView,
-    LoginView,
     Logo,
     CloseModalBtn
   },
   methods: {
-    toggleModal(modalName, isVisible) {
-      if (this.modals.hasOwnProperty(modalName)) {
-        this.modals[modalName] = isVisible
-      }
-    }
   },
   emits: ['close'],
 
